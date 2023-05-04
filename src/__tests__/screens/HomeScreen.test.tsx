905e21b8539e480ca6a282d979b06892e768c5f9
Angga Ari Wijaya <anggadarkprince@gmail.com>
2023-05-04 21:09:10 +0700

    feat(home): add date and day

--- src/__tests__/screens/HomeScreen.test.tsx
+++ src/__tests__/screens/HomeScreen.test.tsx
@@ -7,4 +7,25 @@ describe('HomeScreen', () => {
     const wrapper = render(<HomeScreen />);
     wrapper.getByTestId('home-screen');
   });
+
+  describe('Title section', () => {
+    beforeEach(() => {
+      jest.useFakeTimers('modern');
+      jest.setSystemTime(946684800000); // Saturday, 01 january 2000 00:00:00 UTS
+    });
+
+    afterEach(() => {
+      jest.useRealTimers();
+    });
+
+    test('Should contain current date', () => {
+      const wrapper = render(<HomeScreen />);
+      wrapper.getByText('Jan 01, 2000');
+    });
+
+    test('Should contain current day', () => {
+      const wrapper = render(<HomeScreen />);
+      wrapper.getByText('Saturday');
+    });
+  });
 });
